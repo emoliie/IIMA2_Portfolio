9e5a166825317ce7218d28f19ff7dab7b5e1685b
Emilie Xu <emilie.xu@outlook.com>
2025-03-09 22:39:16 +0100

    :sparkles: Add tape animation

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,4 +1,5 @@
 import StarIcon from "@/assets/icons/star.svg";
+import { Fragment } from "react";
 
 const words = [
   "Creative",
@@ -21,14 +22,21 @@ export const TapeSection = () => {
     <div className="py-16 lg:py-24">
       <div className="bg-gradient-to-r from-red-300 to-purple-400 -rotate-3 -mx-1">
         <div className="flex mask-image-x">
-          <div className="flex flex-none gap-4 py-3">
-            {words.map((word) => (
-              <div key={word} className="inline-flex gap-4 items-center">
-                <span className="text-gray-900 uppercase font-extrabold text-sm">
-                  {word}
-                </span>
-                <StarIcon className="size-6 text-gray-900 -rotate-12" />
-              </div>
+          <div
+            className="flex flex-none gap-4 pr-4 py-3 animate-move-left [animation-duration:30s]"
+            // style={{ transform: "translateX(-750px" }}
+          >
+            {[...new Array(2)].fill(0).map((_, idx) => (
+              <Fragment key={idx}>
+                {words.map((word) => (
+                  <div key={word} className="inline-flex gap-4 items-center">
+                    <span className="text-gray-900 uppercase font-extrabold text-sm">
+                      {word}
+                    </span>
+                    <StarIcon className="size-6 text-gray-900 -rotate-12" />
+                  </div>
+                ))}
+              </Fragment>
             ))}
           </div>
         </div>
